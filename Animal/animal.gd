e433dbdb2fc5188b9e2683557c11e5f5089cf93c
leoxborghi <nabbogianfranco@gmail.com>
2026-03-02 19:12:47 +0100

    8.9 ore

--- Animal/animal.gd
+++ Animal/animal.gd
@@ -4,20 +4,34 @@ class_name Animal
 extends RigidBody2D
 
 
+const DRAG_LIM_MAX: Vector2 = Vector2(0,60)
+const DRAG_LIM_MIN :Vector2 = Vector2(-60,0)
+const IMPULSE_MULT: float = 25.0
+const IMPULSE_MAX: float = 2000.0
+
 
 @onready var label : Label = $Label
 @onready var arrow : Sprite2D = $Arrow
+@onready var stretch_sound: AudioStreamPlayer2D = $StretchSound
+@onready var launch_sound: AudioStreamPlayer2D = $LaunchSound
+
+
 var _start: Vector2 = Vector2.ZERO
 var _drag_start: Vector2 = Vector2.ZERO
 var _dragged_vector: Vector2 = Vector2.ZERO
-
 var _is_dragging: bool = false
+var _arrow_scale_x: float = 0.0
+
+
+func _unhandeld_input(event: InputEvent) -> void:
+	if event.is_action_released("drag")and _is_dragging:
+		call_deferred("start_release")
 
 
 
 func _ready() -> void:
 	_start = position
-
+	_arrow_scale_x = arrow.scale.x
 
 
 func _processed( _delta: float) -> void:
@@ -28,7 +42,13 @@ func _processed( _delta: float) -> void:
 	]
 	debug_str += "is_dragging:%s drag_start:%.0f, %.0f" % [
 		_is_dragging, _drag_start.x, _drag_start.y
-]
+	]
+	debug_str += "dragged_vector:%,0f, %.0f\n" % [
+		
+	]
+	debug_str += "impulse:%.0f" % [
+		calculate_impulse().length()
+	]
 	label.text = debug_str
 
 
@@ -36,10 +56,22 @@ func _physics_process(_delta: float) -> void:
 	if _is_dragging: handle_dragging()
 
 
-func handle_dragging() -> void:
-	_dragged_vector = get_global_mouse_position() - _drag_start
-	position = _start + _dragged_vector
+func calculate_impulse() -> Vector2:
+	return _dragged_vector * IMPULSE_MULT * -1
 
+
+
+func handle_dragging() -> void:
+	var new_dragged_vector: Vector2 = get_global_mouse_position() - _drag_start
+	new_dragged_vector = new_dragged_vector.clamp(DRAG_LIM_MIN, DRAG_LIM_MAX)
+	
+	var diff: Vector2 = new_dragged_vector - _dragged_vector
+	if diff.length() > 0 and !stretch_sound.playing:
+		stretch_sound.play()
+		
+		scale_arrow()
+		_dragged_vector = new_dragged_vector
+	position = _start + _dragged_vector
 
 
 func start_dragging() -> void: 
@@ -48,8 +80,22 @@ func start_dragging() -> void:
 	_drag_start = get_global_mouse_position()
 
 
+func start_release() -> void:
+	launch_sound.play()
+	arrow.hide()
+	_is_dragging = false
+	freeze = false
+	apply_central_impulse(calculate_impulse())
+	
 
-func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+func scale_arrow() -> void:
+	var imp_len: float = calculate_impulse().length()
+	var perc: float = clamp(imp_len / IMPULSE_MAX, 0.0,1.0)
+	arrow.scale.x = lerpf(_arrow_scale_x, _arrow_scale_x * 2,perc)
+	arrow.rotation = (_start - position).angle()
+
+
+func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event.is_action_pressed("drag"):
 		input_event.disconnect(_on_input_event)
 		start_dragging()
