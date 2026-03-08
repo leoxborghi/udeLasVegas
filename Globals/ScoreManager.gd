d9f0dc22687c7705097fbca10a541cba73961c1e
leoxborghi <nabbogianfranco@gmail.com>
2026-03-08 15:34:57 +0100

    gioco finito

--- Globals/ScoreManager.gd
+++ Globals/ScoreManager.gd
@@ -1,19 +1,34 @@
 extends Node
 
 
-const LEVEL_SCORES: LevelScoresResource = preload("res://Scenes/Resources/LevelScores.tres")
+const SCORES_PATH = "user://animals_.res"
 
 
-var level_selected: int = 1
+var level_selected: int = 1:
+	get: return level_selected
+	set (value): level_selected= value
+
+
+var _level_scores: LevelScoresResource = LevelScoresResource.new()
 
 
 
 func _ready() -> void:
-	pass
+	load_scores_from_file()
+
 
 func get_level_best(level: int) -> int:
-	return LEVEL_SCORES.get_level_best(level)
+	return _level_scores.get_level_best(level)
 
 
 func set_score_for_current_level(score: int) -> void:
-	LEVEL_SCORES.try_update_best_score(level_selected, score)
+	_level_scores.try_update_best_score(level_selected, score)
+	save_scores_to_file()
+
+func load_scores_from_file() -> void:
+	if ResourceLoader.exists(SCORES_PATH):
+		_level_scores = load(SCORES_PATH)
+
+
+func save_scores_to_file() -> void:
+	ResourceSaver.save(_level_scores, SCORES_PATH)
